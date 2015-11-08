b41d887265dee7d4d5a2806487484d1e122e22bf
Kostas Lekkas <kostas@lekkas.io>
2015-11-08 20:55:50 +0200

    Solved map

--- map/index.js
+++ map/index.js
@@ -1,11 +1,11 @@
 module.exports = map
 
-function map(list, fn) {
+function map(list, fn, ctx) {
   var i;
 
   var out = [];
   for (i = 0; i < list.length; i++) {
-    out.push(fn(list[i], i, list));
+    out.push(fn.call(ctx, list[i], i, list));
   }
 
   return out;
